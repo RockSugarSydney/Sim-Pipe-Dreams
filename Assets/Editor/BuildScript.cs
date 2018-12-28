b5ed7decd169a017c2a12e39d462bcbbc87b3ecf
AriefPrasetyo911 <arief.prasetyo911@gmail.com>
2018-12-28 13:21:26 +0700

    try build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,23 +1,16 @@
 using UnityEditor;
 using UnityEngine;
-using System.Diagnostics;
-public class BuildScript
+
+public class BuildScript : MonoBehaviour
 {
-    [MenuItem("Build/Build StandaloneWindows")]
-    public static void buildWindows(){
-        //get filename
-        string path     = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
-        string[] levels = new string[] {"Assets/Scenes/PhoneMenu.unity", "Assets/Scenes/Phone.unity", "Assets/Scenes/Chats.unity", "Assets/Scenes/Gallery.unity", "Assets/Scenes/Mail.unity", "Assets/Scenes/Spark.unity", "Assets/Scenes/Jabbr.unity", "Assets/Scenes/Vloggr.unity"};
-
-        //Build player
-        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-
-        // Copy a file from the project folder to the build folder, alongside the built game.
-        FileUtil.CopyFileOrDirectory("Assets/Templates/Readme.txt", path + "Readme.txt");
-
-        // Run the game (Process class from System.Diagnostics).
-        Process proc = new Process();
-        proc.StartInfo.FileName = path + "/BuiltGame.exe";
-        proc.Start();
+    [MenuItem("Build/Build Windows")]
+    public static void MyBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity", "Assets/Scene2.unity" };
+        buildPlayerOptions.locationPathName = "WindowsBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 }
