d17e6c33288c757284a338ac4d38308e403c5259
AriefPrasetyo911 <arief.prasetyo911@gmail.com>
2018-12-28 15:01:02 +0700

    try build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -6,21 +6,38 @@ public class BuildScript : MonoBehaviour
     [MenuItem("Build/Build Windows")]
     public static void MyBuild()
     {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] 
-							{
-								"Assets/Scenes/PhoneMenu.unity", 
-								"Assets/Scenes/Phone.unity", 
-								"Assets/Scenes/Chats.unity", 
-								"Assets/Scenes/Gallery.unity", 
-								"Assets/Scenes/Mail.unity", 
-								"Assets/Scenes/Spark.unity", 
-								"Assets/Scenes/Jabbr.unity", 
-								"Assets/Scenes/Vloggr.unity"
-							};
-        buildPlayerOptions.locationPathName = "WindowsBuild";
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
-        buildPlayerOptions.options = BuildOptions.None;
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        // BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        // buildPlayerOptions.scenes = new[] 
+		// 					{
+		// 						"Assets/Scenes/PhoneMenu.unity", 
+		// 						"Assets/Scenes/Phone.unity", 
+		// 						"Assets/Scenes/Chats.unity", 
+		// 						"Assets/Scenes/Gallery.unity", 
+		// 						"Assets/Scenes/Mail.unity", 
+		// 						"Assets/Scenes/Spark.unity", 
+		// 						"Assets/Scenes/Jabbr.unity", 
+		// 						"Assets/Scenes/Vloggr.unity"
+		// 					};
+        // buildPlayerOptions.locationPathName = "WindowsBuild";
+        // buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        // buildPlayerOptions.options = BuildOptions.None;
+        // BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+		// Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] 
+						{
+							"Assets/Scenes/PhoneMenu.unity", 
+							"Assets/Scenes/Phone.unity", 
+							"Assets/Scenes/Chats.unity", 
+							"Assets/Scenes/Gallery.unity", 
+							"Assets/Scenes/Mail.unity", 
+							"Assets/Scenes/Spark.unity", 
+							"Assets/Scenes/Jabbr.unity", 
+							"Assets/Scenes/Vloggr.unity"
+						};
+						
+		// Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 }
