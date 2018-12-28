22e09abd686a73ca08f84d1e45ab55ecb0b30a10
AriefPrasetyo911 <arief.prasetyo911@gmail.com>
2018-12-28 13:42:16 +0700

    try build script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -7,7 +7,17 @@ public class BuildScript : MonoBehaviour
     public static void MyBuild()
     {
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity", "Assets/Scene2.unity" };
+        buildPlayerOptions.scenes = new[] 
+							{
+								"Assets/Scenes/PhoneMenu.unity", 
+								"Assets/Scenes/Phone.unity", 
+								"Assets/Scenes/Chats.unity", 
+								"Assets/Scenes/Gallery.unity", 
+								"Assets/Scenes/Mail.unity", 
+								"Assets/Scenes/Spark.unity", 
+								"Assets/Scenes/Jabbr.unity", 
+								"Assets/Scenes/Vloggr.unity"
+							};
         buildPlayerOptions.locationPathName = "WindowsBuild";
         buildPlayerOptions.target = BuildTarget.StandaloneWindows;
         buildPlayerOptions.options = BuildOptions.None;
